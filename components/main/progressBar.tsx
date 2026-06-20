762b3ecbe4ce71a2b73b667a41d5a5f0afc040c1
HossainYazdani <hossain.yazdaniii@gmail.com>
2026-06-20 09:31:55 +0330

    Edit nprogress

--- components/main/progressBar.tsx
+++ components/main/progressBar.tsx
@@ -5,6 +5,13 @@ import { usePathname, useSearchParams } from "next/navigation";
 import Nprogress from "nprogress";
 import "nprogress/nprogress.css";
 
+Nprogress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 500,
+  showSpinner: false,
+  trickleSpeed: 200,
+});
 
 export default function ProgressBar () {
   const pathname = usePathname();
